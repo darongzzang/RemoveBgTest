abbf24c988d3f813504e91dae464b5aa59a1277c
darongzzang <101727090+darongzzang@users.noreply.github.com>
2024-09-25 16:08:06 +0900

    Initial Commit

---
+++ RemoveBgTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RemoveBgTest
+//
+//  Created by 김이예은 on 9/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RemoveBgTest/RemoveBgTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoveBgTestApp.swift
+//  RemoveBgTest
+//
+//  Created by 김이예은 on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct RemoveBgTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
