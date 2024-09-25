90059edc6b4f4dd2b587fdf1b1e40e44edbe69fa
darongzzang <101727090+darongzzang@users.noreply.github.com>
2024-09-25 16:42:51 +0900

    마스크 이미지 보이도록 수정

--- RemoveBgTest/ContentView.swift
+++ RemoveBgTest/ContentView.swift
@@ -12,6 +12,8 @@ import CoreImage.CIFilterBuiltins
 struct ContentView: View {
     @State private var image: UIImage?
     @State private var showingImagePicker: Bool = false
+    @State private var maskImage: UIImage? = nil // maskImage 상태 변수 추가
+    @State private var outputImage: UIImage? = nil // 결과 이미지를 저장할 상태 변수 추가
     
     var body: some View {
         VStack {
@@ -20,13 +22,34 @@ struct ContentView: View {
             }, label: {
                 Text("이미지 선택")
             })
-            Image(uiImage: image ?? UIImage.cat)
+            
+            // 선택한 이미지 표시
+            Image(uiImage: image ?? UIImage(named: "cat")!) // 기본 이미지 설정
                 .resizable()
                 .scaledToFit()
+            
+            // 결과 이미지 표시
+            if let output = outputImage {
+                Image(uiImage: output)
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+            }
+            
+            // maskImage 표시
+            if let mask = maskImage { // maskImage가 있을 때만 표시
+                Image(uiImage: mask)
+                    .resizable()
+                    .scaledToFit()
+                    .colorInvert() // 검정-하양으로 보이도록 색상 반전
+                    .padding()
+            }
+            
             Button("누끼 따기") {
                 createSticker()
             }
-        }.sheet(isPresented: $showingImagePicker) {
+        }
+        .sheet(isPresented: $showingImagePicker) {
             ImagePicker(image: $image)
         }
         .padding()
@@ -36,7 +59,7 @@ struct ContentView: View {
     
     private func createSticker() {
         var isLoading = true
-        guard let inputImage = CIImage(image: image ?? UIImage.cat) else {
+        guard let inputImage = CIImage(image: image ?? UIImage(named: "cat")!) else {
             print("Failed to create CIImage")
             return
         }
@@ -48,10 +71,16 @@ struct ContentView: View {
                 }
                 return
             }
+            
+            // maskImage 상태 변수 업데이트
+            DispatchQueue.main.async {
+                self.maskImage = render(ciImage: maskImage) // maskImage를 UIImage로 변환하여 저장
+            }
+            
             let outputImage = apply(mask: maskImage, to: inputImage)
             let image = render(ciImage: outputImage)
             DispatchQueue.main.async {
-                self.image = image
+                self.outputImage = image // 결과 이미지 상태 변수 업데이트
             }
         }
     }
@@ -93,6 +122,6 @@ struct ContentView: View {
         }
         return UIImage(cgImage: cgImage)
     }
+    
     private var processingQueue = DispatchQueue(label: "ProcessingQueue")
 }
-
