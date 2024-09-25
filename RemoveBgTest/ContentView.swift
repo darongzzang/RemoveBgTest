a9c174f898f8ddc28eadd42253c4eb1e93bb4f12
darongzzang <101727090+darongzzang@users.noreply.github.com>
2024-09-25 16:26:02 +0900

    누끼따기 기능까지 완료

--- RemoveBgTest/ContentView.swift
+++ RemoveBgTest/ContentView.swift
@@ -6,19 +6,93 @@
 //
 
 import SwiftUI
+import Vision
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    @State private var image: UIImage?
+    @State private var showingImagePicker: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button(action: {
+                showingImagePicker = true
+            }, label: {
+                Text("이미지 선택")
+            })
+            Image(uiImage: image ?? UIImage.cat)
+                .resizable()
+                .scaledToFit()
+            Button("누끼 따기") {
+                createSticker()
+            }
+        }.sheet(isPresented: $showingImagePicker) {
+            ImagePicker(image: $image)
         }
         .padding()
     }
+    
+    // MARK: - Private
+    
+    private func createSticker() {
+        var isLoading = true
+        guard let inputImage = CIImage(image: image ?? UIImage.cat) else {
+            print("Failed to create CIImage")
+            return
+        }
+        processingQueue.async {
+            guard let maskImage = subjectMaskImage(from: inputImage) else {
+                print("Failed to create mask image")
+                DispatchQueue.main.async {
+                    isLoading = false
+                }
+                return
+            }
+            let outputImage = apply(mask: maskImage, to: inputImage)
+            let image = render(ciImage: outputImage)
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }
+    }
+    
+    private func subjectMaskImage(from inputImage: CIImage) -> CIImage? {
+        let handler = VNImageRequestHandler(ciImage: inputImage)
+        let request = VNGenerateForegroundInstanceMaskRequest()
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+            return nil
+        }
+        
+        guard let result = request.results?.first else {
+            print("No observations found")
+            return nil
+        }
+        do {
+            let maskPixelBuffer = try result.generateScaledMaskForImage(forInstances: result.allInstances, from: handler)
+            return CIImage(cvPixelBuffer: maskPixelBuffer)
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    private func apply(mask: CIImage, to image: CIImage) -> CIImage {
+        let filter = CIFilter.blendWithMask()
+        filter.inputImage = image
+        filter.maskImage = mask
+        filter.backgroundImage = CIImage.empty()
+        return filter.outputImage!
+    }
+    
+    private func render(ciImage: CIImage) -> UIImage {
+        guard let cgImage = CIContext(options: nil).createCGImage(ciImage, from: ciImage.extent) else {
+            fatalError("Failed to render CGImage")
+        }
+        return UIImage(cgImage: cgImage)
+    }
+    private var processingQueue = DispatchQueue(label: "ProcessingQueue")
 }
 
-#Preview {
-    ContentView()
-}
---
+++ RemoveBgTest/ImagePicker.swift
@@ -0,0 +1,44 @@
+//
+//  ImagePicker.swift
+//  RemoveBgTest
+//
+//  Created by 김이예은 on 9/25/24.
+//
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .photoLibrary
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
